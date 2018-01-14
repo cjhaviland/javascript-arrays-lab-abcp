8536d367360af0604fdfc28e4efaa207be06f6de
CJ Haviland <cj.haviland@yahoo.com>
2018-01-14 03:20:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyprependKitten(name){
+  kittens.unshift(name);
+}
